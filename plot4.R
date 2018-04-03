935e6099cfe36a7a1c00d0affdb9bfa5e6b61a90
mtsy34 <mtsy34@gmail.com>
2018-04-03 17:17:20 +0800

    Made changes to plot4

--- plot4.R
+++ plot4.R
@@ -14,10 +14,10 @@ plot(data$Time, data$Voltage, xlab = "datetime", ylab = "Voltage", type = "l")
 with(data, plot(Time, Sub_metering_1, type = "l", xlab="", ylab = "Energy sub metering"))
 lines(data$Time, data$Sub_metering_2, col = "red")
 lines(data$Time, data$Sub_metering_3, col = "blue")
-legend("topright", lty = 1, lwd = 3, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5)
 
 ## Additional new plot
 plot(data$Time, data$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 
-dev.copy(png, "plot4.png")
+dev.copy(png, "plot4.png", width = 480, height = 480)
 dev.off()
